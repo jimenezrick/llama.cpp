890652cbaa24a31f4f2efb8c0b0153ac40ee0f1c
Ricardo Catalinas Jiménez <r@untroubled.be>
2025-08-31 20:08:21 +0100

    Improve comments

--- llama-run-server.sh
+++ llama-run-server.sh
@@ -8,10 +8,12 @@ source $CUDAENV
 cmake -B build -DGGML_CUDA=ON -DLLAMA_CURL=ON
 cmake --build build --config Release -j8
 
-# Download model at: ~/.cache/llama.cpp
+# - Download model at: ~/.cache/llama.cpp
 # ./build/bin/llama-cli -hf ggml-org/gemma-3n-E4B-it-GGUF:Q8_0
+#
+# (More models at: https://huggingface.co/ggml-org)
 
-# CLI
+# - Chat:
 # ./build/bin/llama-cli -m models/ggml-org_gemma-3n-E4B-it-GGUF_gemma-3n-E4B-it-Q8_0.gguf -ngl 9999 --color --multiline-input
-# Server
+# - Server:
 ./build/bin/llama-server -m models/ggml-org_gemma-3n-E4B-it-GGUF_gemma-3n-E4B-it-Q8_0.gguf -ngl 9999

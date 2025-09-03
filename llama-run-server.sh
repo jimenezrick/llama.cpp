e0040f160b76f64b5f9ed015e81a75d6c0c0ba3d
Ricardo Catalinas Jiménez <r@untroubled.be>
2025-09-03 16:07:57 +0100

    Flags to adjust context size

--- llama-run-server.sh
+++ llama-run-server.sh
@@ -2,6 +2,10 @@
 
 set -eu
 
+: ${MODEL:=models/ggml-org_gemma-3n-E4B-it-GGUF_gemma-3n-E4B-it-Q8_0.gguf}
+: ${CONTEXT:=0} # Model default
+: ${GPU_LAYERS:=9999}
+
 CUDAENV=~/bin/cuda.env
 source $CUDAENV
 
@@ -16,4 +20,4 @@ cmake --build build --config Release -j8
 # - Chat:
 # ./build/bin/llama-cli -m models/ggml-org_gemma-3n-E4B-it-GGUF_gemma-3n-E4B-it-Q8_0.gguf -ngl 9999 --color --multiline-input
 # - Server:
-./build/bin/llama-server -m models/ggml-org_gemma-3n-E4B-it-GGUF_gemma-3n-E4B-it-Q8_0.gguf -ngl 9999
+./build/bin/llama-server --model $MODEL  --gpu-layers $GPU_LAYERS --ctx-size $CONTEXT
